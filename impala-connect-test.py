fddd83a12a4ce1370a00e10b34912ed1563a2241
41xu <xu_sy11111@mail.dlut.edu.cn>
2019-07-13 14:25:53 +0800

    事件分析

--- impala-connect-test.py
+++ impala-connect-test.py
@@ -1,7 +1,8 @@
 from impala.dbapi import connect
+import datetime
 
-def random_sample(percent):
-    # 抽样总表
+
+def random_sample(percent):  # 抽样总表
     create_string = "create table random_sample as select * from event_export where user_id%" + str(percent) + "=1"
     cur = conn.cursor()
     cur.execute('use rawdata')
@@ -9,13 +10,15 @@ def random_sample(percent):
     cur.execute(create_string)
 
 
-def sample(event_ids, quary):  # event_ids->tuple; quary->[year,month]
+def funnel(event_ids, quary):  # event_ids->tuple; quary->[year,month]
     # quary处理
     from_month = "'" + quary[0] + "-" + quary[1] + "-01 00:00:0.000000000'"
     if int(quary[1]) < 12:
         to_month = "'" + quary[0] + "-" + "{:0>2d}".format(int(quary[1]) + 1) + "-01 00:00:00.000000000'"
     else:
         to_month = "'" + str(int(quary[0]) + 1) + "-01-01 00:00:00.000000000'"
+
+    count0=count1=count2=count3=0 # count默认为0
 
     # 抽取只含查询状态的数据
 
@@ -43,17 +46,58 @@ def sample(event_ids, quary):  # event_ids->tuple; quary->[year,month]
     cur.execute(create_string)
     data = cur.fetchall()
     count1, count2, count3 = data[0][0], data[0][1], data[0][2]
-    print(count0, count1, count2, count3)
-    # count1=cur.fetchall()[0][0]
-    # print(count0,count1)
-    # cur.execute('select count(user_id) from '
-    #             '(select * from sample_funnel where time>'+str(quary_month))'
-    # print(data)
-    # cur.execute('drop table if exists sample')
-    # cur.execute('create table sample as select * from event_export where user_id%500 =1 ')
-    # cur.execute('select event_id, user_id, time from (select event_id, user_id, time from sample where event_id in event_ids order by user_id,time ')
-    #
-    # data=cur.fetchall()
+    return count0, count1, count2, count3
+
+
+def event(from_time,to_time,event_id,feature,group): # from_time: "2019-01-01", event_id: str, feature: str, group: str
+    features={
+        "0":"", # 总次数
+        "1":"", # 总人数
+        "2":"", # 去重人数
+        "3":"", # 人均次数
+        # "4":"", # 平均事件时长
+    }
+    groups={
+        "0":"", # 广告系列来源分组
+        "1":"", # 是否首次访问分组
+    }
+    from_time="'"+from_time+" 00:00:00.000000000'"
+    to_time="'"+to_time+" 00:00:00.000000000'"
+
+    create_string="create view sample_event as select * from event_export where event_id="+event_id+" and "+\
+        from_time+" <time and time< "+to_time
+
+    cur.execute('use rawdata')
+    cur.execute('drop view if exists rawdata.sample_event')
+    cur.execute(create_string)
+
+    features["0"]="select count(time),day from sample_event group by day"
+    features["1"]="select count(user_id),day from sample_event group by day"
+    features["2"]="select count(distinct user_id),day from sample_event group by day "
+    features["3"]="select count(time)/count(distinct user_id),day from sample_event group by day"
+    # features["4"]="select sum(p__event_duration)/count(p__event_duration),day from sample_event group by day"
+
+    groups["0"]="select count(p_utm_source),p_utm_source,day from sample_event group by day,p_utm_source"
+    groups["1"]="select count(p_is_first_time),p_is_first_time,day from sample_event group by day,p_is_first_time"
+
+
+    cur.execute(features[feature])
+    feature_result=cur.fetchall()
+    feature_result=[list(x) for x in feature_result]
+    for x in feature_result:
+        x[1]=str(datetime.datetime.fromtimestamp(x[1]*86400))[:10]
+
+    cur.execute(groups[group])
+    group_result=cur.fetchall()
+    group_result=[list(x) for x in group_result]
+    for x in group_result:
+        x[2]=str(datetime.datetime.fromtimestamp(x[2]*86400))[:10]
+
+    # 用户选择的event_id可能在限制时间内为None，在演示时为了避免老师认为我们的功能写的是错的，可以在impala中执行这句话
+    # select count(p_is_first_time),p_is_first_time,day from event_export where '2019-02-01 00:00:00.000000000'<time
+    # and time<'2019-02-07 00:00:00.000000000' group by day,p_is_first_time;
+
+    return feature_result,group_result
 
 
 if __name__ == '__main__':
@@ -61,4 +105,12 @@ if __name__ == '__main__':
     cur = conn.cursor()
     event_ids = (5, 19, 28, 1)
     quary = ["2019", "02"]
-    sample(event_ids, quary)
+    # count=funnel(event_ids, quary)
+    # print(count)
+    from_time="2019-02-01"
+    to_time="2019-02-05"
+    event_id="28"
+    feature="3"
+    group="1"
+    features,groups=event(from_time,to_time,event_id,feature,group)
+    print(features,groups)
